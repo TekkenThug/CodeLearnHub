f600906149b7c12977e28e6a5c65f822b6ec1d49
Vadim Ignatov <ignatov0131@gmail.com>
2023-01-29 12:56:32 +0300

    feat: create landing category

---
+++ frontend/components/landing/LandingCategory.vue
@@ -0,0 +1,133 @@
+<template>
+  <section class="landing-category">
+    <div class="container">
+      <h2 class="landing-category__title">
+        Наиболее <span class="blue">популярные категории</span>
+      </h2>
+
+      <p class="landing-category__subtitle">
+        Various versions have evolved over the years, sometimes by accident,
+      </p>
+
+      <ul class="landing-category__list">
+        <li
+          v-for="item in categories"
+          :key="item.icon"
+          class="landing-category__item"
+        >
+          <div class="landing-category__item-left">
+            <Icon :name="item.icon" />
+
+            <span>{{ item.title }}</span>
+          </div>
+
+          <div class="landing-category__item-right">
+            <Icon name="carbon:arrow-up-right" />
+          </div>
+
+          <NuxtLink :to="item.link" class="landing-category__item-link" />
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+const categories = ref<{ icon: string, title: string, link: string }[]>([
+  {
+    icon: 'carbon:pen',
+    title: 'Дизайн',
+    link: '/'
+  }
+])
+</script>
+
+<style lang="sass" scoped>
+.landing-category
+  padding: 80px 0
+  background-color: $second-primary
+
+  &__title
+    margin-bottom: 16px
+    font-size: 48px
+    line-height: 56px
+    font-weight: 600
+
+    & > .blue
+      color: $second-primary-500
+
+  &__subtitle
+    font-size: 20px
+    line-height: 28px
+    color: $gray-500
+
+  &__list
+    display: flex
+    flex-wrap: wrap
+    margin-top: 50px
+    margin-bottom: -24px
+    margin-right: -24px
+
+  &__item
+    @include trans
+    display: flex
+    align-items: center
+    justify-content: space-between
+    position: relative
+    width: calc((100% - (24px * 3)) / 4)
+    margin-bottom: 24px
+    margin-right: 24px
+    background-color: $white
+    border-radius: 22px
+    border: 2px solid transparent
+    padding: 16px 24px
+
+    &:hover
+      border-color: $secondary-500
+      box-shadow: 0px 12px 48px rgba(255, 194, 122, .26)
+
+      .landing-category__item-right
+        background-color: $secondary-500
+        box-shadow: -4px 4px 20px rgba(242, 206, 18, .12)
+
+        .icon
+          color: $white
+
+  &__item-link
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
+
+  &__item-left
+    display: flex
+    align-items: center
+    margin-right: 8px
+
+    .icon
+      width: 38px
+      height: 38px
+      margin-right: 16px
+      opacity: .38
+
+    span
+      font-size: 20px
+      line-height: 28px
+      font-weight: 500
+
+  &__item-right
+    @include trans
+    display: flex
+    justify-content: center
+    align-items: center
+    width: 44px
+    height: 44px
+    border-radius: 50%
+
+    .icon
+      @include trans
+      width: 24px
+      height: 24px
+      color: $secondary-500
+</style>
