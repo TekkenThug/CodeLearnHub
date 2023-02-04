915ce4cec966cde6a0b0327fc4daa992ca7d484f
Vadim Ignatov <v.ignatov@idaproject.com>
2023-02-04 20:06:56 +0300

    feat: create landing page (withot backend binding)

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,13 +1,26 @@
 <template>
   <main class="landing">
     <LandingStart />
+
+    <LandingFacts />
+
+    <LandingCourses />
+
+    <LandingCategory />
+
+    <LandingInstructors />
+
+    <LandingFeedback />
   </main>
 </template>
 
 <script setup lang="ts">
 import LandingStart from '~/components/landing/LandingStart.vue'
-
-const searchValue = ref('')
+import LandingFacts from '~/components/landing/LandingFacts.vue'
+import LandingCategory from '~/components/landing/LandingCategory.vue'
+import LandingCourses from '~/components/landing/LandingCourses.vue'
+import LandingInstructors from '~/components/landing/LandingInstructors.vue'
+import LandingFeedback from '~/components/landing/LandingFeedback.vue'
 </script>
 
 <style lang="sass">
