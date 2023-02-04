381e8a309eec8d1880f18d5e0d43ccd4ef63ee40
Vadim Ignatov <v.ignatov@idaproject.com>
2023-02-04 19:12:37 +0300

    feat: create gradient button component

---
+++ frontend/components/ui/buttons/UiButtonGradient.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="ui-button-gradient">
+    <slot />
+  </button>
+</template>
+
+<style lang="sass">
+.ui-button-gradient
+  @include trans
+  position: absolute
+  display: flex
+  justify-content: center
+  align-items: center
+  background: linear-gradient(180deg, #FFC27A 0%, #FFA337 100%), linear-gradient(180deg, #FFB0BA 0%, #F9475D 100%), #FFFFFF
+  background-size: 70px 70px
+  border-radius: 100px
+  padding: 12px 24px
+  color: $white
+
+  &:hover
+    background-position-y: -10px
+</style>
