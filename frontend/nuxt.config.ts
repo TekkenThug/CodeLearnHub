2338a6c2ff952049082276a2451ca60e72736da9
Vadim Ignatov <v.ignatov@idaproject.com>
2023-02-05 23:39:12 +0300

    feat: change page title

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+
   typescript: {
     strict: true
   },
+
   app: {
     head: {
+      title: 'eDex',
+
       link: [
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap' }
       ]
