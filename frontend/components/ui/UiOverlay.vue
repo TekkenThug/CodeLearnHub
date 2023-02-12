17f1a25b9d1fe5df3604466d6e8de6839836ca66
Vadim Ignatov <v.ignatov@idaproject.com>
2023-02-12 14:27:28 +0300

    feat: create ui overlay

---
+++ frontend/components/ui/UiOverlay.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="ui-overlay">
+    <UiLoader class="ui-overlay__loader" />
+  </div>
+</template>
+
+<style lang="sass">
+.ui-overlay
+  position: absolute
+  display: flex
+  justify-content: center
+  align-items: center
+  background-color: rgba($gray-100, .8)
+
+  &__loader
+    position: relative
+</style>
