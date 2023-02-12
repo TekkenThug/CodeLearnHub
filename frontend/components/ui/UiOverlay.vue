56da92d44d41b95079fff011a4f9ab85bf1f8cc2
Vadim Ignatov <v.ignatov@idaproject.com>
2023-02-12 14:40:25 +0300

    feat: add transition, type and zindex for overlay

--- frontend/components/ui/UiOverlay.vue
+++ frontend/components/ui/UiOverlay.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="ui-overlay">
-    <UiLoader class="ui-overlay__loader" />
-  </div>
+  <Transition name="fade">
+    <div v-if="props.isLoading" class="ui-overlay">
+      <UiLoader class="ui-overlay__loader" />
+    </div>
+  </Transition>
 </template>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<{ isLoading?: boolean }>(), {
+  isLoading: false
+})
+</script>
 
 <style lang="sass">
 .ui-overlay
   position: absolute
+  z-index: 1
   display: flex
   justify-content: center
   align-items: center
