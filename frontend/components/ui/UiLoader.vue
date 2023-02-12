e6c96ecb919c843e14ae08838ec7395795ddfc43
Vadim Ignatov <v.ignatov@idaproject.com>
2023-02-12 14:27:09 +0300

    feat: create ui loader

---
+++ frontend/components/ui/UiLoader.vue
@@ -0,0 +1,37 @@
+<template>
+  <span :class="['ui-loader', `ui-loader--${props.type}`]" />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  type?: 'primary'
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  type: 'primary'
+})
+</script>
+
+<style lang="sass">
+.ui-loader
+  width: 48px
+  height: 48px
+  border-width: 5px
+  border-style: solid
+  border-bottom-color: transparent
+  border-radius: 50%
+  display: inline-block
+  box-sizing: border-box
+  animation: rotation 1s linear infinite
+
+  &--primary
+    border-color: $blue
+    border-bottom-color: transparent
+
+@keyframes rotation
+  0%
+    transform: rotate(0deg)
+
+  100%
+    transform: rotate(360deg)
+</style>
