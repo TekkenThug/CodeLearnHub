413c164f945ba8420e3d4494f1c1512fd84943c0
Vadim Ignatov <ignatov0131@gmail.com>
2023-02-19 14:56:24 +0300

    feat: create AuthController

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Requests\StoreUserRequest;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     /**
      * Register new user
      */
-    public function register(StoreUserRequest $request) {
+    public function register(StoreUserRequest $request)
+    {
         $validated = $request->validated();
 
         $newUser = User::create([
@@ -27,6 +30,47 @@ class AuthController extends Controller
             'data' => [
                 'user' => $newUser
             ]
+        ]);
+    }
+
+    /**
+     * Login user
+     */
+    public function login(Request $request)
+    {
+        $credentials  = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'error' => 'Неверный логин или пароль'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $request->session()->regenerate();
+
+        return response()->json([
+            'message' => 'Вход произведен успешно',
+            'data' => [
+                'user' => Auth::user()
+            ]
+        ]);
+    }
+
+    /**
+     * Logout user
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json([
+            'message' => 'Вы вышли из системы'
         ]);
     }
 }
