6ea3e1a8c1c6ae34e44be74bdb39d0a75b28ad19
Vadim Ignatov <ignatov0131@gmail.com>
2023-02-19 17:02:43 +0300

    feat: create auth middleware

---
+++ frontend/middleware/auth.ts
@@ -0,0 +1,9 @@
+import { useUserStore } from '~/stores/user'
+
+export default defineNuxtRouteMiddleware(() => {
+  const store = useUserStore()
+
+  if (!store.isAuth) {
+    return navigateTo('/')
+  }
+})
