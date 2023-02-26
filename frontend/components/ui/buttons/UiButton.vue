69d8dfed9a814c1ffae6c082d30dd66f8e04a441
Vadim Ignatov <ignatov0131@gmail.com>
2023-02-26 20:24:40 +0300

    feat: add loading state for button

--- frontend/components/ui/buttons/UiButton.vue
+++ frontend/components/ui/buttons/UiButton.vue
@@ -1,9 +1,21 @@
 <template>
   <button
-    :class="['ui-button', `ui-button--${props.type}`]"
+    :class="[
+      'ui-button',
+      `ui-button--${props.type}`,
+      {
+        'ui-button--is-loading': isLoading
+      }
+    ]"
     :disabled="disabled"
     @click="$emit('click')"
   >
+    <Transition name="fade" :duration="100">
+      <div v-if="isLoading" class="ui-button__loader">
+        <UiLoader />
+      </div>
+    </Transition>
+
     <slot />
   </button>
 </template>
@@ -13,25 +25,37 @@ defineEmits(['click'])
 
 interface Props {
   type?: 'primary'
-  disabled?: boolean
+  disabled?: boolean,
+  isLoading?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
   type: 'primary',
-  disabled: false
+  disabled: false,
+  isLoading: false
 })
 </script>
 
 <style lang="sass">
 .ui-button
   @include trans
+  position: relative
   font-weight: 500
   border-radius: 7px
   padding: 12px 24px
+  overflow: hidden
+
+  &--is-loading
+    pointer-events: none
 
   &--primary
     color: $white
     background-color: $second-primary-500
+
+    .ui-button__loader
+      .ui-loader
+        border-color: $white
+        border-bottom-color: transparent
 
     &:hover
       background-color: $second-primary-600
@@ -39,4 +63,22 @@ const props = withDefaults(defineProps<Props>(), {
     &:disabled
       background-color: $second-primary-200
       cursor: not-allowed
+
+  &__loader
+    display: flex
+    justify-content: center
+    align-items: center
+    position: absolute
+    top: 0
+    left: 0
+    z-index: 1
+    width: 100%
+    height: 100%
+    background-color: inherit
+    pointer-events: none
+
+    .ui-loader
+      width: 24px
+      height: 24px
+      border-width: 2px
 </style>
