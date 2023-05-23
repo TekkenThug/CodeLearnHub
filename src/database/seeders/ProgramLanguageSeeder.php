72b57094067ba819614d020c3fe014b87ee1e948
Vadim Ignatov <ignatov0131@gmail.com>
2023-05-23 23:31:18 +0300

    feat: add seeder for programming language

---
+++ src/database/seeders/ProgramLanguageSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProgramLanguageSeeder extends Seeder
+{
+    private $languages = [
+        'PHP',
+        'JavaScript',
+        'Python',
+        'Java'
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach ($this->languages as $language)
+        {
+            DB::table('program_languages')->insert([
+                'name' => $language,
+            ]);
+        }
+    }
+}
