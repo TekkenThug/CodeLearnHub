0585a691e18bc27e79ed3e3f27b563303430d2e7
Vadim Ignatov <ignatov0131@gmail.com>
2023-05-27 21:17:34 +0300

    chore: add indents for http

--- frontend/composables/useHttp.ts
+++ frontend/composables/useHttp.ts
@@ -6,11 +6,11 @@ import axios from 'axios'
 const baseURL = 'http://localhost:80/'
 
 const httpInstance = axios.create({
-  baseURL,
-  withCredentials: true,
-  headers: {
-    Accept: 'application/json'
-  }
+    baseURL,
+    withCredentials: true,
+    headers: {
+        Accept: 'application/json'
+    }
 })
 
 export const useHttp = () => httpInstance
