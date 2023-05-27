f0b7cc5236392832fa53659a4a03c9b458e38df9
Vadim Ignatov <ignatov0131@gmail.com>
2023-05-27 20:54:41 +0300

    feat: install gsap and svg sprite

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,43 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+    ssr: false,
 
-  typescript: {
-    strict: true
-  },
+    typescript: {
+        strict: true
+    },
 
-  runtimeConfig: {
-    public: {
-      baseURL: process.env.NODE_ENV === 'production' ? process.env.PROD_API_URL : process.env.DEV_API_URL
-    }
-  },
-
-  app: {
-    head: {
-      title: 'eDex',
-
-      link: [
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Unbounded:wght@300;400;500;600&display=swap' }
-      ]
-    }
-  },
-
-  css: [
-    'normalize.css',
-    'vue-final-modal/style.css',
-    '@splidejs/vue-splide/css',
-    'vue-toastification/dist/index.css'
-  ],
-
-  modules: ['nuxt-icon'],
-
-  vite: {
-    css: {
-      preprocessorOptions: {
-        sass: {
-          additionalData: '@use "@/assets/sass/_global.sass" as *'
+    runtimeConfig: {
+        public: {
+            baseURL: process.env.NODE_ENV === 'production' ? process.env.PROD_API_URL : process.env.DEV_API_URL
         }
-      }
+    },
+
+    app: {
+        head: {
+            title: 'eDex',
+
+            link: [
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Unbounded:wght@300;400;500;600&display=swap' }
+            ]
+        }
+    },
+
+    css: [
+        'normalize.css',
+        'vue-final-modal/style.css',
+        '@splidejs/vue-splide/css',
+        'vue-toastification/dist/index.css'
+    ],
+
+    modules: [
+        '@nuxtjs/svg-sprite',
+        'nuxt-icon'
+    ],
+
+    vite: {
+        css: {
+            preprocessorOptions: {
+                sass: {
+                    additionalData: '@use "@/assets/sass/_global.sass" as *'
+                }
+            }
+        }
     }
-  }
 })
