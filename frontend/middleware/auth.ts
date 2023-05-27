334da9eb825e189d707185b102ffa8769e59251d
Vadim Ignatov <ignatov0131@gmail.com>
2023-05-27 22:29:35 +0300

    feat: add indents

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -1,9 +1,9 @@
 import { useUserStore } from '~/stores/user'
 
 export default defineNuxtRouteMiddleware(() => {
-  const store = useUserStore()
+    const store = useUserStore()
 
-  if (!store.isAuth) {
-    return navigateTo('/')
-  }
+    if (!store.isAuth) {
+        return navigateTo('/')
+    }
 })
