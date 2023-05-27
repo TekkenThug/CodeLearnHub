0fb1f164dc5300e83897b0261b934319f8c9a779
Vadim Ignatov <ignatov0131@gmail.com>
2023-05-27 20:54:58 +0300

    feat: create question for landing

---
+++ frontend/components/pages/landing/questions/LandingQuestions.vue
@@ -0,0 +1,60 @@
+<template>
+    <section class="section">
+        <div class="container">
+            <div :class="$style.wrapper">
+                <h2 :class="$style.title">
+                    Вопросы и ответы
+                </h2>
+
+                <ul :class="$style.questionList">
+                    <LandingQuestionsItem
+                        v-for="item in questions"
+                        :key="item.question"
+                        v-bind="item"
+                        :class="$style.questionItem"
+                    />
+                </ul>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+import LandingQuestionsItem from '~/components/pages/landing/questions/LandingQuestionsItem.vue'
+
+const questions = ref([
+    {
+        question: 'Чему здесь можно научиться?',
+        answer: 'Вы можете познакомиться с основами языков JavaScript, PHP, Java, Python и других'
+    },
+    {
+        question: 'Как проходит обучение?',
+        answer: 'Курсы программирования состоят из тематических блоков, в каждом есть несколько уроков. Проходите уроки последовательно: сначала изучите теорию, потом выполните практическое упражнение прямо в браузере – система проверит его автоматически.'
+    },
+    {
+        question: 'Как начать обучение?',
+        answer: 'Достаточно просто зарегистрироваться. Регистрация на платформе бесплатная, после регистрации вы получите доступ ко всем курсам.'
+    }
+])
+</script>
+
+<style lang="sass" module>
+.wrapper
+    display: flex
+    flex-direction: column
+    align-items: center
+    max-width: 700px
+    margin: 0 auto
+
+.title
+    @include h1
+    margin-bottom: $offset-m
+    color: $blue-main
+
+.questionList
+    width: 100%
+
+.questionItem
+    &:not(:last-child)
+        margin-bottom: $offset-m
+</style>
---
+++ frontend/components/pages/landing/questions/LandingQuestionsItem.vue
@@ -0,0 +1,62 @@
+<template>
+    <li :class="$style.LandingQuestionsItem">
+        <div
+            :class="$style.questionItemQuestion"
+            @click="isOpen = !isOpen"
+        >
+            {{ question }}
+
+            <Icon
+                name="bi:chevron-down"
+                size="16"
+                :class="[$style.icon, { [$style['_rotated']]: isOpen }]"
+            />
+        </div>
+
+        <UiExpander :is-open="isOpen">
+            <div :class="$style.questionItemAnswer">
+                {{ answer }}
+            </div>
+        </UiExpander>
+    </li>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+    question: string,
+    answer: string
+}>()
+
+const isOpen = ref(false)
+</script>
+
+<style lang="sass" module>
+.LandingQuestionsItem
+    padding: $offset-s $offset-m
+    border-radius: $r-m
+    background-color: $white
+    border: 1px solid $blue-main
+    box-shadow: 0 20px 30px rgba(91, 194, 208, 0.2)
+
+.icon
+    @include trans
+    position: absolute
+    right: 0
+    top: 50%
+    transform: translateY(-50%)
+
+    &._rotated
+        transform: translateY(-50%) rotate(-180deg)
+
+.questionItemQuestion
+    @include h3
+    position: relative
+    padding-right: 20px
+    color: $blue-dark-card
+    cursor: pointer
+    user-select: none
+
+.questionItemAnswer
+    margin-top: $offset-s
+    line-height: 24px
+</style>
