2f04e410d8754aa24f90e1b880a46ab72bf6eddd
Vadim Ignatov <ignatov0131@gmail.com>
2023-05-27 21:04:27 +0300

    feat: replace old components

---
+++ frontend/components/pages/landing/common/LandingEntrance.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="landing-entrance">
+    <h2 class="landing-entrance__title" v-html="title" />
+
+    <p v-if="subtitle" class="landing-entrance__subtitle">
+      {{ props.subtitle }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+  subtitle?: string
+}>()
+</script>
+
+<style lang="sass" scoped>
+.landing-entrance
+  margin-bottom: 50px
+
+  &__title
+    margin-bottom: 16px
+    font-size: $font-heading
+    line-height: $lh-heading
+    font-weight: 600
+
+    & :deep(.blue)
+      color: $second-primary-500
+
+  &__subtitle
+    font-size: $font-body-large
+    line-height: $lh-body-large
+    color: $gray-500
+</style>
