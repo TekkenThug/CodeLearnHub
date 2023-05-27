73ccb402732df59cbea1414db6343f8ab5b72e41
Vadim Ignatov <ignatov0131@gmail.com>
2023-05-27 20:45:29 +0300

    feat: create test flight

---
+++ frontend/components/common/TestFlight.vue
@@ -0,0 +1,32 @@
+<template>
+    <div :class="$style.TestFlight">
+        <div class="container">
+            Cайт находится в тестовом режиме.
+            По всем техническим вопросам обращаться <a href="mailto:ignatov0131@gmail.com">ignatov0131@gmail.com</a>.
+            Версия сайта - {{ version }}
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const version = ref('0.1.0')
+</script>
+
+<style module lang="sass">
+.TestFlight
+    position: sticky
+    padding: 8px 0
+    width: 100%
+    background-color: $blue-main
+    text-align: center
+    color: #fff
+    font-size: 12px
+    font-weight: 600
+    bottom: 0
+
+    @include responsive($mobile)
+        position: static
+
+    a
+        text-decoration: underline
+</style>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="layout-default">
-    <TheHeader />
+    <div class="layout-default">
+        <TheHeader />
 
-    <div class="layout-default__content">
-      <slot />
+        <div class="layout-default__content">
+            <slot />
+        </div>
+
+        <TheFooter />
+
+        <TestFlight />
     </div>
-
-    <TheFooter />
-  </div>
 </template>
 
 <script setup lang="ts">
 import TheHeader from '~/components/common/header/TheHeader.vue'
-import TheFooter from '~~/components/common/TheFooter.vue'
+import TheFooter from '~/components/common/TheFooter.vue'
+import TestFlight from '~/components/common/TestFlight.vue'
 </script>
 
 <style lang="sass">
