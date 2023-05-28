63873e74e6b08e50ad06669b844f4112065ce400
Vadim Ignatov <ignatov0131@gmail.com>
2023-05-28 20:28:29 +0300

    feat: rework user store

--- frontend/stores/user.ts
+++ frontend/stores/user.ts
@@ -2,36 +2,87 @@ import { defineStore } from 'pinia'
 
 interface User {
   id: number | null,
-  firstName: string,
-  secondName: string,
+  nickname: string,
   email: string,
+  avatar?: string,
+}
+
+interface UpdateUser {
+    email?: string,
+    nickname?: string,
+    password?: string
 }
 
 export const useUserStore = defineStore('user', () => {
-  const http = useHttp()
+    const http = useHttp()
 
-  const user = ref<User | null>(null)
-  const isAuth = ref<boolean>(false)
+    const user = ref<User | null>(null)
+    const isAuth = ref<boolean>(false)
 
-  const setUserData = (userData: User, authState: boolean) => {
-    isAuth.value = authState
-    user.value = userData
-  }
+    const setUserData = (userData: User | null, authState: boolean) => {
+        isAuth.value = authState
+        user.value = userData
+    }
 
-  const logout = () => {
-    isAuth.value = false
-    user.value = null
-  }
+    const logout = () => {
+        isAuth.value = false
+        user.value = null
+    }
 
-  const getUserInfo = () => {
-    return http.get('/api/v1/user').then(({ data }) => {
-      if (!data) {
-        return
-      }
+    const getUserInfo = () => {
+        return http.get('/api/v1/user').then(({ data }) => {
+            if (!data) {
+                return
+            }
 
-      setUserData(data, true)
-    }).catch(() => null)
-  }
+            setUserData(data, true)
+        }).catch(() => null)
+    }
 
-  return { user, isAuth, setUserData, logout, getUserInfo }
+    const updateUserData = (data: UpdateUser) => {
+        if (!user.value) {
+            return
+        }
+
+        return http.put(`/api/v1/users/${user.value.id}`, data).then(({ data }) => data)
+    }
+
+    const uploadAvatar = (avatar: Blob) => {
+        if (!user.value) {
+            return
+        }
+
+        const form = new FormData()
+
+        form.append('image', avatar)
+        form.append('id', user.value.id)
+
+        return http.post('/api/v1/upload/avatar', form, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then(({ data }) => data)
+    }
+
+    const deleteUser = () => {
+        if (!user.value) {
+            return
+        }
+
+        return http.delete(`/api/v1/users/${user.value.id}`).then(({ data }) => {
+            setUserData(null, false)
+            return data
+        })
+    }
+
+    return {
+        user,
+        isAuth,
+        setUserData,
+        logout,
+        getUserInfo,
+        updateUserData,
+        deleteUser,
+        uploadAvatar,
+    }
 })
