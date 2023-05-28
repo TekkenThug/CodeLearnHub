565217faeec05e8cc6043ca3c96cf267a19dc946
Vadim Ignatov <ignatov0131@gmail.com>
2023-05-28 20:23:15 +0300

    feat: add avatar to table

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('nickname')->unique();
             $table->string('password');
+            $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
