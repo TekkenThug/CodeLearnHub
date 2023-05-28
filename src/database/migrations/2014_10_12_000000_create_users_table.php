b2cae5ee4161dafe48e9f8531a05bbb32f7635f0
Vadim Ignatov <ignatov0131@gmail.com>
2023-05-28 17:48:09 +0300

    feat: remove role id from users table

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('nickname')->unique();
             $table->string('password');
-            $table->unsignedBigInteger('role_id');
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
