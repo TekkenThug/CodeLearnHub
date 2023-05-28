d670b53831675fdae418278d9d9874951b9cd91c
Vadim Ignatov <ignatov0131@gmail.com>
2023-05-28 17:47:41 +0300

    feat: rename migration

--- src/database/migrations/2023_05_21_104242_role_user.php
+++ src/database/migrations/2023_05_21_104242_role_user.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('user_role', function (Blueprint $table) {
+        Schema::create('role_user', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('role_id');
 
