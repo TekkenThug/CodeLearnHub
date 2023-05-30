acd0d925c2134a90151c49ce5b633d2412c95401
Vadim Ignatov <ignatov0131@gmail.com>
2023-05-30 23:11:49 +0300

    feat: create author button

---
+++ frontend/components/pages/courses/create/AuthorAddButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <div
+        :class="$style.AuthorAddButton"
+        @click="$emit('click')"
+    >
+        <Icon
+            :class="$style.icon"
+            name="bi:plus-circle-fill"
+        />
+
+        <span>{{ caption }}</span>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const emit = defineEmits<{(e: 'click'): void }>()
+const props = defineProps<{
+    caption: string,
+}>()
+</script>
+
+<style lang="sass" module>
+.AuthorAddButton
+    @include h3
+    @include trans
+    display: flex
+    justify-content: center
+    align-items: center
+    color: $blue-main
+    border: 2px dashed $blue-main
+    padding: 16px 20px
+    border-radius: $r-m
+    user-select: none
+    cursor: pointer
+
+    @include hover
+        border-color: $blue-dark
+        color: $blue-dark
+
+    span
+        margin-left: $offset-m
+</style>
