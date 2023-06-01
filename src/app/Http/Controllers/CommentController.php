2b2bde285ae7afd07d0a392a6cc48ebec80c206a
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-02 00:05:27 +0300

    feat: create comment controller

---
+++ src/app/Http/Controllers/CommentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+
+class CommentController extends Controller
+{
+    public function post(Request $request)
+    {
+        $authorId = $request->user()->id;
+        $lessonId = $request->input('lessonId');
+        $content = $request->input('text');
+
+        Comment::create([
+            'user_id' => $authorId,
+            'lesson_id' => $lessonId,
+            'content' => $content,
+        ]);
+
+        return response()->json([
+            'message' => 'Комментарий успешно опубликован'
+        ]); 
+    }
+}
