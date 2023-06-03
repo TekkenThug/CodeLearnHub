3293f5ff4eef23c1df33c80bf1c2e71a1db02722
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-03 21:32:56 +0300

    feat: update comment model

--- src/app/Models/Comment.php
+++ src/app/Models/Comment.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Lesson;
 
 class Comment extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'lesson_id',
+        'content',
+    ];
+
+    public function lesson()
+    {
+        return $this->belongsTo(Lesson::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
