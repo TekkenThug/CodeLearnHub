5f69606823102c0087ceb7bf1619a080cb72d19f
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-04 13:56:19 +0300

    fix: update role middleware

--- src/app/Http/Middleware/CheckRole.php
+++ src/app/Http/Middleware/CheckRole.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class CheckRole
 {
@@ -15,7 +16,7 @@ class CheckRole
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next, $role)
     {
         if (!Auth::check() || !Auth::user()->hasRole($role)) {
             return response()->status(Response::HTTP_FORBIDDEN);
