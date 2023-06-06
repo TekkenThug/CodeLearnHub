cfd249fdbfa3c48086f60ca5f607166506abbd6f
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-06 22:26:06 +0300

    feat: get lesson info

--- src/app/Http/Controllers/LessonController.php
+++ src/app/Http/Controllers/LessonController.php
@@ -10,33 +10,86 @@ use App\Models\Comments;
 
 class LessonController extends Controller
 {
-    public function get(Request $request)
+    /**
+     * Получение данных об уроке для изучения
+     */
+    public function getForTeach(Request $request)
     {
-        if ($request->has('forTeach'))
-        {
-            $course = Course::find($request->input('courseId'));
+        $courseId = $request->input('courseId');
+        $course = Course::find($courseId);
+        $moduleOrder = $request->input('moduleOrder');
+        $lessonOrder = $request->input('lessonOrder');
 
-            if (!$course->is_active) return response()->json([]);
+        if (!$course->is_active) return response()->json([]);
 
-            $module = Module::where('course_id', $course->id)
-            ->where('order', $request->input('moduleOrder'))
-            ->first();
+        $existedRecord = $course->users()->where('user_id', $request->user()->id);
 
-            $lesson = Lesson::where('module_id', $module->id)
-            ->where('order', $request->input('lessonOrder'))
-            ->first();
+        // Если не записан
+        if (!$existedRecord->exists()) {
+            response()->json([
+                'message' => 'Вас нет на курсе'
+            ], 403);
+        }
 
-            $comments = $lesson->comments;
-            $module = $lesson->module;
-            $module->course;
+        $row = $existedRecord
+        ->select('current_module_id', 'current_lesson_id')
+        ->first();
+        $module = null;
+        $lesson = null;
+        $currentModule = Module::where('id', $row->current_module_id)->first();
 
-            foreach ($comments as $comment) {
-                $comment->user;
-            }
+        // Если пытаются взять следующий непройденный модуль
+        if ($currentModule->order < $moduleOrder) {
+            $module = $currentModule;
+            $lesson = Lesson::where('id', $row->current_lesson_id)->first();
 
             return response()->json([
-                'data' => $lesson,
-            ]);
+                'data' => [
+                    'redirect' => [
+                        'module_order' => $module->order,
+                        'lesson_order' => $lesson->order,
+                    ],
+                ],
+            ]); 
+        } else {
+            $module = Module::where('course_id', $course->id)
+            ->where('order', $moduleOrder)
+            ->first();
+
+            $currentLesson = Lesson::where('id', $row->current_lesson_id)->first();
+
+            // Если пытаются взять непройденный урок
+            if ($currentLesson->order < $lessonOrder) {
+                $lesson = $currentLesson;
+
+                return response()->json([
+                    'data' => [
+                        'redirect' => [
+                            'module_order' => $module->order,
+                            'lesson_order' => $lesson->order,
+                        ],
+                    ],
+                ]); 
+            } else {
+                $lesson = Lesson::where('module_id', $module->id)
+                ->where('order', $lessonOrder)
+                ->first();
+            }
         }
+
+        $comments = $lesson->comments;
+        $module = $lesson->module;
+        $module->course;
+
+        foreach ($comments as $comment) {
+            $comment->user;
+        }
+
+        return response()->json([
+            'data' => [
+                'lesson' => $lesson,
+            ],
+        ]);
+    }
     }
 }
