d6bc2acec048ee6b21fe06f21e5cc10de2f0d62e
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-07 23:29:18 +0300

    feat: create program test for js

---
+++ src/app/Services/ProgramTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class ProgramTest
+{
+	public function run($language, $code, $testCode) 
+	{
+		if ($language === 'JavaScript') {
+			return $this->javaScriptCode($code, $testCode);
+		}
+	}
+
+	public function javaScriptCode($code, $testCode)
+	{
+		$testConfig = uniqid('script_', true) . '.config.json';
+        $fileWithCode = uniqid('script_', true) . '.js';
+
+		$fileWithTest = uniqid('script_', true) . '.test.js';
+		Storage::disk('local')->put($fileWithTest, $code . "\n" . $testCode);
+		Storage::disk('local')->put($testConfig, '{
+			"bail": 1,
+			"verbose": true
+		  }');
+
+		$command = 'jest' . ' ' . storage_path('app/' . $fileWithTest) . ' ' . '--config='. storage_path('app/' . $testConfig);
+
+		$process = Process::fromShellCommandline($command);
+        $process->run();
+
+		$output = $process->getOutput();
+        $error = $process->getErrorOutput();
+
+		Storage::disk('local')->delete($fileWithTest);
+		Storage::disk('local')->delete($testConfig);
+
+		return strpos($error, 'PASS') === 0;
+	}
+}
