39b89251989a276a1637fea34796e2e105ea852b
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-10 10:22:34 +0300

    feat: create AvatarImage

---
+++ frontend/components/common/avatar/AvatarImage.vue
@@ -0,0 +1,37 @@
+<template>
+    <img
+        v-if="path"
+        :src="path"
+        :class="[$style.avatar, { [$style['_rounded']]: rounded }]"
+        alt="Avatar"
+    >
+
+    <img
+        v-else
+        src="~/assets/images/common/default-avatar.png"
+        :class="[$style.avatar, { [$style['_rounded']]: rounded }]"
+        alt="Avatar"
+    >
+</template>
+
+<script setup>
+const props = defineProps({
+    path: {
+        type: String,
+        default: ''
+    },
+
+    rounded: {
+        type: Boolean,
+        default: true
+    }
+})
+</script>
+
+<style lang="sass" module>
+.avatar
+    display: block
+
+    &._rounded
+        border-radius: 50%
+</style>
