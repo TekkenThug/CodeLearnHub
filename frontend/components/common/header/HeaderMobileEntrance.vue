8dea088928cd5272bf890586f0e084792db3a891
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-10 11:00:08 +0300

    feat: create HeaderMobileEntrance

---
+++ frontend/components/common/header/HeaderMobileEntrance.vue
@@ -0,0 +1,34 @@
+<template>
+    <button
+        :class="$style.entranceButton"
+        @click="$emit('open')"
+    >
+        Войти
+
+        <Icon
+            name="bi:door-open"
+            :class="$style.entranceButtonIcon"
+        />
+    </button>
+</template>
+
+<script setup>
+const emits = defineEmits(['open'])
+</script>
+
+<style lang="sass" module>
+.entranceButton
+    display: none
+    align-items: center
+    margin-left: auto
+    padding: 0
+    font-weight: 500
+    color: $blue-dark-card
+    font-family: $font-title
+
+    @include responsive($mobile)
+        display: flex
+
+.entranceButtonIcon
+    margin-left: $offset-xxs
+</style>
