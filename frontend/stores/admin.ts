2fc138fa0fb41b17d00ac31a20a4356904e2b33e
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-10 10:23:04 +0300

    feat: update url for getting users

--- frontend/stores/admin.ts
+++ frontend/stores/admin.ts
@@ -4,7 +4,7 @@ export const useAdminStore = defineStore('admin', () => {
     const http = useHttp()
 
     const getUserList = () => {
-        return http.get('/api/v1/users').then(({ data }) => data)
+        return http.get('/api/v1/admin/users').then(({ data }) => data)
     }
 
     return {
