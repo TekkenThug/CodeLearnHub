6233afe80ccbef78c896796fe28dd9d520c26d8b
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-11 19:03:32 +0300

    feat: change page title

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
 
     app: {
         head: {
-            title: 'eDex',
+            title: 'CodeLearnHub',
 
             link: [
                 { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Unbounded:wght@300;400;500;600&display=swap' }
