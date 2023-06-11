54eee13a3cd6147a43497ea331cd158f64d11338
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-11 19:03:15 +0300

    feat: create language controller

--- frontend/components/pages/admin/AdminLanguagesControl.vue
+++ frontend/components/pages/admin/AdminLanguagesControl.vue
@@ -1,11 +1,118 @@
 <template>
-    <div></div>
+    <div>
+        <div :class="$style.block">
+            <h2 :class="$style.title">
+                Создать язык
+            </h2>
+
+            <div :class="$style.addRow">
+                <UiInput
+                    v-model="title"
+                    :class="$style.input"
+                    placeholder="Названия языка программирования"
+                />
+
+                <UiButton
+                    :is-loading="addLanguageIsLoading"
+                    :disabled="!title"
+                    @click="sendLanguage"
+                >
+                    Добавить
+                </UiButton>
+            </div>
+        </div>
+
+        <div :class="$style.block">
+            <h2 :class="$style.title">
+                Список языков
+            </h2>
+
+            <transition name="fade" mode="out-in">
+                <UiLoader v-if="languagesIsLoading" />
+
+                <UiTable
+                    v-else-if="languages && languages.length"
+                    :head="tableHead"
+                    :rows="languages"
+                />
+
+                <h3 v-else>
+                    Языков нет
+                </h3>
+            </transition>
+        </div>
+    </div>
 </template>
 
-<script>
+<script setup>
+import { useAdminStore } from '~/stores/admin'
+import UiInput from '~/components/ui/UiInput'
+import UiLoader from '~/components/ui/UiLoader'
+import UiButton from '~/components/ui/buttons/UiButton'
+import UiTable from '~/components/ui/table/UiTable'
 
+const admin = useAdminStore()
+const notify = useToast()
+const title = ref('')
+const languages = ref([])
+const languagesIsLoading = ref(true)
+const addLanguageIsLoading = ref(false)
+
+const tableHead = [
+    {
+        title: 'ID',
+        key: 'id'
+    },
+    {
+        title: 'Название',
+        key: 'name'
+    }
+]
+
+onBeforeMount(async() => {
+    const { data } = await admin.getLanguages()
+    languages.value = data
+    languagesIsLoading.value = false
+})
+
+const sendLanguage = async() => {
+    if (languagesIsLoading.value) {
+        return
+    }
+
+    addLanguageIsLoading.value = true
+
+    try {
+        const { message, data } = await admin.postLanguage(title.value)
+
+        notify.success(message)
+        languages.value.push(data)
+        title.value = ''
+    } catch (e) {
+        if (e?.response?.data) {
+            notify.error(e.response.data.error || e.response.data.message)
+        }
+    } finally {
+        addLanguageIsLoading.value = false
+    }
+}
 </script>
 
-<style scoped>
+<style lang="sass" module>
+.title
+    @include h2
+    color: $blue-main
+    margin-bottom: $offset-xs
 
+.block
+    &:not(:last-child)
+        margin-bottom: $offset-xl
+
+.addRow
+    width: 100%
+    display: flex
+
+.input
+    margin-right: $offset-s
+    flex-grow: 1
 </style>
