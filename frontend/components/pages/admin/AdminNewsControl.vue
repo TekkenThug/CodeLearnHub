60c72a8a0813d7524388d2b00da5bfbc8066691f
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-11 14:19:59 +0300

    feat: create AdminNewsControl

--- frontend/components/pages/admin/AdminNewsControl.vue
+++ frontend/components/pages/admin/AdminNewsControl.vue
@@ -1,13 +1,77 @@
 <template>
+    <div>
+        <div>
+            <h2 :class="$style.title">
+                Создать новость
+            </h2>
 
+            <UiInput
+                v-model="title"
+                placeholder="Заголовок новости"
+            />
+
+            <UiEditor
+                :class="$style.editor"
+                :clear="isClear"
+                placeholder="Содержимое новости"
+                @change="setContent"
+                @clear="isClear = false"
+            />
+
+            <UiButton
+                :disabled="disabled"
+                @click="sendNews"
+            >
+                Создать новость
+            </UiButton>
+        </div>
+    </div>
 </template>
 
-<script>
-export default {
-    name: 'AdminNewsControl'
+<script setup>
+import { useAdminStore } from '~/stores/admin'
+import UiInput from '~/components/ui/UiInput'
+import UiEditor from '~/components/ui/UiEditor'
+import UiButton from '~/components/ui/buttons/UiButton'
+
+const admin = useAdminStore()
+const notify = useToast()
+const title = ref('')
+const text = ref(null)
+const isClear = ref(false)
+
+const setContent = (value) => {
+    text.value = value
+}
+
+const disabled = computed(() => {
+    return !title.value || !text.value || !text.value.length
+})
+
+const sendNews = async() => {
+    if (disabled.value) {
+        return
+    }
+
+    try {
+        const { message } = await admin.postNews({ title: title.value, text: JSON.stringify(text.value) })
+
+        notify.success(message)
+
+        isClear.value = true
+        title.value = ''
+    } catch (e) {
+        notify.error(e.response.data.error || e.response.data.message)
+    }
 }
 </script>
 
-<style scoped>
+<style lang="sass" module>
+.title
+    @include h2
+    color: $blue-main
+    margin-bottom: $offset-xs
 
+.editor
+    margin: $offset-xs 0
 </style>
