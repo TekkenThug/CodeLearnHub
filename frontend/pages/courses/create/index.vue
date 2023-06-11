5975764b0c03a90fadee6d98a05bfdc99b1d9e11
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-11 19:04:55 +0300

    feat: create course create page

---
+++ frontend/pages/courses/create/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <main class="page">
+        <AuthorCreate v-if="isAuthor" />
+
+        <AuthorBanner v-else />
+    </main>
+</template>
+
+<script setup>
+import { useUserStore } from '~/stores/user'
+import AuthorBanner from '~/components/pages/courses/create/AuthorBanner'
+import AuthorCreate from '~/components/pages/courses/create/AuthorCreate'
+
+const userStore = useUserStore()
+
+const isAuthor = computed(() => userStore.isAuthor())
+
+definePageMeta({
+    middleware: ['auth']
+})
+</script>
