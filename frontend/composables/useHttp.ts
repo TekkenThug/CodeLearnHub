c3e3352cada92a91f84c6118b9928fd75f175738
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-17 00:32:05 +0300

    Revert "feat: change localhost"
    This reverts commit 14ee91681f62dec20b18e40876ef6800f48307e2.

--- frontend/composables/useHttp.ts
+++ frontend/composables/useHttp.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 /**
  * TODO: Replace this on env
  */
-const baseURL = 'http://localhost:8000'
+const baseURL = 'http://217.25.88.146/'
 
 const httpInstance = axios.create({
     baseURL,
