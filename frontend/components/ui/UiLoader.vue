33c812aea2ac8d2dbc70ad4c4b6b97703c4a4d55
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-16 22:10:37 +0300

    Revert "feat: change color for test"
    This reverts commit 35fad70ac0e4d9bb9ac13b02601bc890e3b5c24f.

--- frontend/components/ui/UiLoader.vue
+++ frontend/components/ui/UiLoader.vue
@@ -25,7 +25,7 @@ const props = withDefaults(defineProps<Props>(), {
   animation: rotation 1s linear infinite
 
   &--primary
-    border-color: red
+    border-color: $blue-main
     border-bottom-color: transparent
 
   &--secondary
